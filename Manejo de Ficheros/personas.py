9811dbc8659765f53dc4b159ff681cae3b48350d
Tusitala73 <SU-CORREO-ELECTRONICO>
2019-05-16 06:44:12 +0200

    terminado el ejercio 1 archivos

--- Manejo de Ficheros/personas.py
+++ Manejo de Ficheros/personas.py
@@ -1,18 +1,14 @@
 from io import open
-class Personas:
-    def __init__ (self,n,nombre,apellido,nacimiento):
-        self.n = n
-        self.nombre = nombre
-        self.apellido = apellido
-        self.nacimiento = nacimiento
-    def __str__(self):
-        return "{} {} con fecha de nacimiento{} e ID {} ".format(self.nombre,apellido,nacimiento,n)
-
-personas = open('p.txt', 'r')
+ 
+personas = open('p.txt', 'r', encoding="utf8")
 lpersonas = personas.readlines()
 personas.close()
 
+listapersona = []
 for p in lpersonas:
-    persona = Personas([p])
+    campos = p.replace("\n", "").split(";")
+    persona = {"id":campos[0], "nombre":campos[1], "apellido":campos[2], "nacimiento":campos[3]}
+    listapersona.append(persona)
 
-print(persona)
+for per in listapersona:
+    print("id{}  {} {} nacicido en {}".format(per["id"], per["nombre"], per["apellido"], per["nacimiento"]))
