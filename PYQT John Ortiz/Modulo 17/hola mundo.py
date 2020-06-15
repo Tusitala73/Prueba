55a092c0e7989dc49cfa0332e7cadaa8a0a5283b
Tusitala73 <SU-CORREO-ELECTRONICO>
2020-06-15 13:19:20 +0200

    master

---
+++ PYQT John Ortiz/Modulo 17/hola mundo.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+
+
+class VentanaPrincipal(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setWindowTitle('hola, PyQt5')
+
+        self.setFixedSize(500, 400)
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    ventana = VentanaPrincipal()
+
+    ventana.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
